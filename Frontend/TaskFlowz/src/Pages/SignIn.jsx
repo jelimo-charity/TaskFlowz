6f07a5034a10339cdb0a97d9b64cab6867242ddc
jelimo-charity <charityjelimo893@gmail.com>
2023-07-07 09:06:28 +0300

    added signup page

--- Frontend/TaskFlowz/src/Pages/SignIn.jsx
+++ Frontend/TaskFlowz/src/Pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import './signin.css'
+import { useNavigate } from 'react-router-dom'
 import signinImg from '../assets/signIn-img.png'
 function SignIn() {
+  const navigate = useNavigate()
   return (
     <div className='signinContainer'>
       <div className="col1">
@@ -11,19 +13,18 @@ function SignIn() {
         <input type="email" />
         <h4>Enter Password</h4>
         <input type="password" /><br/>
-        <input id='signinbtn' type="submit" />
+        <input id='signinbtn' type="submit" onClick={()=> navigate('/dashboard')}/>
       </form>
       </div>
       <div className="col2">
-        <h2>Welcome to TaskFlowz!</h2>
+        <h2>TaskFlowz!</h2>
         <p>The ultimate task management solution designed to revolutionize the way you handle your tasks.
-          <br/> <span>With TaskFlowz, Simplify and Streamline your task management with ease.</span>
+          <br/> <span>Simplify and Streamline your task management with ease.</span>
         </p>
-        <p>With TaskFlowz, you can effortlessly manage and prioritize your tasks, collaborate with 
-          team members,<br/> and stay on top of deadlines. Say goodbye to the chaos of scattered to-do
-           lists and embrace a streamlined workflow that empowers you to achieve more. 
+        <p>With TaskFlowz, you can effortlessly manage and prioritize your tasks,<br/> and stay on top of deadlines. Say goodbye to the chaos of scattered to-do
+           lists and embrace a streamlined<br/> workflow that empowers you to achieve more. <br/>
            Experience the power of TaskFlowz and unlock your productivity potential today.</p>
-        <h4>Have No Account? <button>Sign Up</button> </h4>
+        <h4>Have No Account? <button onClick = {() => navigate('/signup')} >Sign Up</button> </h4>
       </div>
     </div>
   )
--- Frontend/TaskFlowz/src/Pages/SignUp.jsx
+++ Frontend/TaskFlowz/src/Pages/SignUp.jsx
@@ -1,8 +1,23 @@
-
+import './signup.css'
+import signupImg from "../assets/signUp-img.png"
+import { useNavigate } from 'react-router-dom'
 function SignUp() {
+  const navigate = useNavigate();
   return (
-    <div>
-        <h1>Sign up</h1>
+    <div className="signupContainer">
+      <div className="signupImg">
+        <img id="signupImg" src={signupImg} alt="page-image" />
+      </div>
+      <div className="signupForm">
+        <h1>Sign Up</h1>
+        <h3>Enter username:</h3>
+        <input type="text" />
+        <h3>Enter email:</h3>
+        <input type="email" />
+        <h3>Enter password:</h3>
+        <input type="password" /><br/>
+        <input id="signupBtn" type="submit" onClick = {() => navigate('/')}/>
+      </div>
       
     </div>
   )
