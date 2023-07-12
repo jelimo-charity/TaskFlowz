c72b97959edbd5d4141d10ec0bb8a6d5dfbf00f1
jelimo-charity <charityjelimo893@gmail.com>
2023-07-11 22:39:42 -0700

    change

--- Frontend/TaskFlowz/src/Components/Topbar.jsx
+++ Frontend/TaskFlowz/src/Components/Topbar.jsx
@@ -5,14 +5,16 @@ import { FaTasks } from 'react-icons/fa'
 import { IoNotifications } from 'react-icons/io5'
 function Topbar() {
   return (
-  
+  <div>
+
  <div className='topbar'>
-    <input type="text" placeholder="Search..." className="topbarInput" />
+  
     <h2 className='linkItem'> <FaUser/>Profile</h2>
     <h2 className="linkItem"> <MdNoteAdd/>Add New Task</h2>
     <h2 className='linkItem'> <FaTasks/> My Tasks</h2>
     <h2 className='linkItem'> <IoNotifications/> Notifications</h2>
-    
+    <h2 className='linkItem'> <FaTasks/> Taskboard</h2>
+ </div>
  </div>
 
   )
